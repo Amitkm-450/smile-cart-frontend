69f43b2a113e98b2cf9d5da81704adfa725c36ed
BigBinary <bigbinary@BigBinarys-MacBook-Pro-2.local>
2025-02-04 15:29:17 +0530

    Implemented Axios interceptors to set http headers and to transform response

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -12,8 +12,9 @@ const Product = () => {
 
   const fetchProduct = async () => {
     try {
-      const response = await productsApi.show();
-      setProduct(response.data);
+      const products = await productsApi.show();
+      console.log(products);
+      setProduct(products);
     } catch (error) {
       console.log("An error occurred:", error);
     } finally {
@@ -25,9 +26,8 @@ const Product = () => {
     fetchProduct();
   }, []);
 
-  const { name, description, mrp, offer_price, image_urls, image_url } =
-    product;
-  const totalDiscounts = mrp - offer_price;
+  const { name, description, mrp, offerPrice, imageUrls, imageUrl } = product;
+  const totalDiscounts = mrp - offerPrice;
   const discountPercentage = ((totalDiscounts / mrp) * 100).toFixed(1);
 
   if (isLoading) {
@@ -49,13 +49,10 @@ const Product = () => {
       <div className="mt-16 flex gap-4">
         <div className="w-2/5">
           <div className="flex justify-center gap-16">
-            {isNotNil(image_urls) ? (
-              <Carousel
-                imageUrls={append(image_url, image_urls)}
-                title={name}
-              />
+            {isNotNil(imageUrls) ? (
+              <Carousel imageUrls={append(imageUrl, imageUrls)} title={name} />
             ) : (
-              <img alt={name} className="w-48" src={image_url} />
+              <img alt={name} className="w-48" src={imageUrl} />
             )}
           </div>
         </div>
@@ -63,7 +60,7 @@ const Product = () => {
           <Typography>{description}</Typography>
           <Typography>MRP: {mrp}</Typography>
           <Typography className="font-semibold">
-            Offer price: {offer_price}
+            Offer price: {offerPrice}
           </Typography>
           <Typography className="font-semibold text-green-600">
             {discountPercentage}% off
