07098cb0e018b9498b31b6cba3e99ad96b962957
BigBinary <bigbinary@BigBinarys-MacBook-Pro-2.local>
2025-02-05 15:09:42 +0530

    Added feature to add products to cart

--- src/components/ProductList/index.jsx
+++ src/components/ProductList/index.jsx
@@ -4,7 +4,7 @@ import productsApi from "apis/products";
 import useDebounce from "hooks/useDebounce";
 import { Search } from "neetoicons";
 import { Spinner, Input, NoData } from "neetoui";
-import { isEmpty } from "ramda";
+import { isEmpty, without } from "ramda";
 
 import ProductListItem from "./ProductListItem";
 
@@ -14,7 +14,7 @@ const ProductList = () => {
   const [products, setProducts] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [searchKey, setSearchKey] = useState("");
-
+  const [cartItems, setCartItems] = useState([]);
   const debouncedSearchKey = useDebounce(searchKey);
 
   const fetchProducts = async () => {
@@ -42,10 +42,18 @@ const ProductList = () => {
     );
   }
 
+  const toggleIsInCart = slug =>
+    setCartItems(prevCartItems =>
+      prevCartItems.includes(slug)
+        ? without([slug], cartItems)
+        : [slug, ...cartItems]
+    );
+
   return (
     <div className="flex h-screen flex-col">
       <div className="flex flex-col">
         <Header
+          cartItemsCount={cartItems.length}
           shouldShowBackButton={false}
           title="Smile Cart"
           actionBlock={
@@ -63,7 +71,12 @@ const ProductList = () => {
         ) : (
           <div className="grid grid-cols-2 justify-items-center gap-y-8 p-4 md:grid-cols-3 lg:grid-cols-4">
             {products.map(product => (
-              <ProductListItem key={product.slug} {...product} />
+              <ProductListItem
+                key={product.slug}
+                {...product}
+                isInCart={cartItems.includes(product.slug)}
+                toggleIsInCart={() => toggleIsInCart(product.slug)}
+              />
             ))}
           </div>
         )}
