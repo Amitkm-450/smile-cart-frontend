d9736cddc4cff89b3780796f94625cfe7f3d25be
BigBinary <bigbinary@BigBinarys-MacBook-Pro-2.local>
2025-02-07 18:27:06 +0530

    Integrated Formik form into the checkout page

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Cart from "components/Cart";
+import Checkout from "components/Checkout";
 import Product from "components/Product/index";
 import ProductList from "components/ProductList/index";
 import { Route, Switch, Redirect } from "react-router-dom";
@@ -9,6 +10,7 @@ import PageNotFound from "./components/commons/PageNotFound";
 const App = () => (
   <Switch>
     <Route exact component={Cart} path={routes.cart} />
+    <Route exact component={Checkout} path={routes.checkout} />
     <Route exact component={Product} path={routes.products.show} />
     <Route exact component={ProductList} path={routes.products.index} />
     <Redirect exact from={routes.root} to={routes.products.index} />
